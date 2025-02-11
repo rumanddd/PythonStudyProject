d3b8941a4b6fbf01ce7119bf8375909e4de22e5c
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-11 23:51:49 +0100

    added whileStatements and anAnnoyingWhileLoop

---
+++ whileStatements.py
@@ -0,0 +1,6 @@
+eggs = 0
+while eggs < 10:
+    print('Hello There!')
+    eggs = eggs + 1
+# This code gives the variable eggs a value of 0 and then runs a while loop
+# that will print 'Hello There!' 10 times but adds a value of +1 to the variable eggs.
