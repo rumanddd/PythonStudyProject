cf764dbc4b64e6667d972b3ef7765f3e33632f68
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-12 23:36:25 +0100

    changed number of guests file and added a file explaining the 'for' loop function and range() function

---
+++ forLoop&range()Function.py
@@ -0,0 +1,3 @@
+print('My name is ')
+for i in range(5): # range(5) is the same as range(0, 5)
+    print('Jimmy Five Times (' + str(i) + ')') # This will print the string plus the value of i
--- numberOfGuests.py
+++ numberOfGuests.py
@@ -2,7 +2,7 @@
 name = ''
 
 # Ask for the user's name
-while not name:
+while not name: # Meaning if the name variable is empty it shall keep asking
     print('Enter your name:')
     name = input()  # Keeps asking until a valid name is entered
 
@@ -12,7 +12,7 @@ print('How many guests will you have?')
 numofGuests = int(input())
 
 # Check if numofGuests is greater than 0
-if numofGuests:
+if numofGuests: # numOfGuests != 0 could also be used
         print('Be sure to have enough room for all your guests.')
 
 print('Done')
