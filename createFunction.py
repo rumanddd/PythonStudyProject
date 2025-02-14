18685d233458c7afc9996a11177a87f9351f4f25
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-14 14:42:24 +0100

    learn how to create a function

---
+++ createFunction.py
@@ -0,0 +1,10 @@
+def hello(): # Defines a function named hello()
+    print("Hello")
+    print("Howdy!!")
+    print("Hi There!!!")
+    # This code is executed when the function is called, not when the function is first defined
+
+hello()
+hello()
+hello()
+# The hello() lines after the function are function calls
