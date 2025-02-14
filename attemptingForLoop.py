0bdb69e8882714eca065abe235c3cb7ee5150bc8
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-14 03:12:17 +0100

    whileLoop that counts from 1 to 10

---
+++ attemptingForLoop.py
@@ -0,0 +1,2 @@
+for number in range(1, 11): # Starts at 1, stops before 11
+    print(number)
---
+++ attemptingWhileLoop.py
@@ -0,0 +1,6 @@
+number = 1 # Start at 1 (instead of 0, since we want 1 to 10)
+while number < 11: # Run until number reaches 10
+    print(number)
+    number = number + 1 # Add 1 to number
+    if number == 11:
+        break
