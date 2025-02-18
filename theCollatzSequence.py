334a417d33f6ec948abe9fe06d1f6185b9763755
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-18 19:18:31 +0100

    added theCollatzSequence remember to change it as you progress

---
+++ theCollatzSequence.py
@@ -0,0 +1,17 @@
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    else:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+n = int(input("Give me a number: "))
+steps = 0  # Counter to track the number of steps
+
+while n != 1:
+    n = collatz(n)
+    steps += 1  # Increment counter
+
+print(f"It took {steps} steps to reach 1.")
