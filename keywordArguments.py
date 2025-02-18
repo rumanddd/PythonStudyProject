20dd5127404677a93f14a85f9f91d8374b44068a
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-18 15:56:04 +0100

    Keyword Arguments and print function

---
+++ keywordArguments.py
@@ -0,0 +1,4 @@
+print('Hello', end=' ') # You can set the end keyword argument to change the newline character to a different string.
+print('World')
+
+print('dog', 'cat', 'mouse', sep=' & ') # You can set the sep keyword argument to change the separator between the strings.
