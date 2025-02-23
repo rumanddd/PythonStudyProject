3344e28bb0a77dfdd9247820ebd4cea74de53560
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-23 19:42:40 +0100

    for loops

---
+++ forLoopIndexingList.py
@@ -0,0 +1,3 @@
+supplies = ['pencils', 'staplers', 'pins', 'glues' ] # Define a list called 'supplies' with different items
+for i in range(len(supplies)): # Loop through the range of numbers from 0 to the length of 'supplies' - 1
+    print('Index '+ str(i) + ' in supplies are ' + supplies[i]) # Print the index and the corresponding item in the list
---
+++ forLoopWithList.py
@@ -0,0 +1,3 @@
+for i in range(4):
+    print(i)
+# for i in [0, 1, 2, 3]: is the same outcome
