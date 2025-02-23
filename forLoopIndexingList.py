2a9daa8860081ece65647e1ebb762d79652c2faf
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-23 20:39:27 +0100

    .

--- forLoopIndexingList.py
+++ forLoopIndexingList.py
@@ -1,3 +1,4 @@
 supplies = ['pencils', 'staplers', 'pins', 'glues' ] # Define a list called 'supplies' with different items
-for i in range(len(supplies)): # Loop through the range of numbers from 0 to the length of 'supplies' - 1
+for i, item in enumerate(supplies): # Loop through the range of numbers from 0 to the length of 'supplies' - 1
     print('Index '+ str(i) + ' in supplies are ' + supplies[i]) # Print the index and the corresponding item in the list
+    # The enumerate() function is useful if you need both the item and the item’s index in the loop’s block.
