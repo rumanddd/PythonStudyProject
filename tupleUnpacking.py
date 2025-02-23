8f26b4544a4a4b493163f6fe0bfddd26efc4137e
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-23 20:39:19 +0100

    .

---
+++ tupleUnpacking.py
@@ -0,0 +1,11 @@
+# This will show you how to assign multiple values to a variables
+# This is called tuple unpacking
+cat = ['Beige', 'Tall', 'Energetic'] # This is a list
+color, height, energy = cat # This will assign the values to the variables
+print(color, height, energy)
+
+# This is the same but just longer
+# cat = ['fat', 'gray', 'loud']
+# size = cat[0]
+# color = cat[1]
+# disposition = cat[2]
