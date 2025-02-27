931a7ac1b9e8539c7ea848d748992b09f0109427
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-28 00:04:59 +0100

    .

---
+++ sequenceDataTypes.py
@@ -0,0 +1,8 @@
+name = 'Ulviyya'
+print(name[0]) # Indexing using positive indexing
+print(name[-2]) # Negative indexing
+print(name[0:4]) # Slicing using : operator and positive indexing
+print(name[:4]) # Slicing using : operator and negative indexing
+print(name[2:]) # Slicing using : operator and positive indexing
+print('Ul' in name) # Membership test using in keyword and not in keyword
+for i in name: print('* * * ' + i + ' * * *') # Iterating over a string using a for loop and indexing the string character by character
