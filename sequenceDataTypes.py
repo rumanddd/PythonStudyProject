feb2ee0236b10932a112cec8de789ae1f0991505
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-28 00:07:55 +0100

    .

--- sequenceDataTypes.py
+++ sequenceDataTypes.py
@@ -5,4 +5,4 @@ print(name[0:4]) # Slicing using : operator and positive indexing
 print(name[:4]) # Slicing using : operator and negative indexing
 print(name[2:]) # Slicing using : operator and positive indexing
 print('Ul' in name) # Membership test using in keyword and not in keyword
-for i in name: print('* * * ' + i + ' * * *') # Iterating over a string using a for loop and indexing the string character by character
+for i in name: print('️❤❤❤ ' + i + ' ❤❤❤') # Iterating over a string using a for loop and indexing the string character by character
