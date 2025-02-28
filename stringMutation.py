6c8e687bd2dc1c1a69c2cc09d5be35ce154dae3a
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-28 01:11:46 +0100

    .

--- stringMutation.py
+++ stringMutation.py
@@ -1,4 +1,5 @@
 #The proper way to “mutate” a string is to use slicing and concatenation to build a new string by copying from parts of the old string.
 name = 'Pluto a cat'
 newName = name[0:6] + 'the' + name[7:12]
+print(name)
 print(newName)
