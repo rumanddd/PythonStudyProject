da2e0d789b929990b6a78b8348e3bef237ae13e1
Ruman Dhaliwal <rumanddd@gmail.com>
2025-02-28 03:18:00 +0100

    .

--- idFunction.py
+++ idFunction.py
@@ -1,6 +1,8 @@
-eggs = ['cat', 'dog'] # This creates a new list.
-print(id(eggs))
-eggs.append('moose') # append() modifies the list "in place".
-print(id(eggs)) # eggs still refers to the same list as before.
-eggs = ['bat', 'rat', 'cow'] # This creates a new list, which has a new id
-print(id(eggs)) # eggs now refers to a completely different list.
+eggs = ['cat', 'dog']  # This creates a new list with elements ['cat', 'dog']
+print(id(eggs))  # Prints the memory address of the list "eggs"
+
+eggs.append('moose')  # append() modifies the existing list by adding 'moose' at the end
+print(id(eggs))  # The memory address remains the same because lists are mutable
+
+eggs = ['bat', 'rat', 'cow']  # This creates a new list and assigns it to the variable "eggs"
+print(id(eggs))  # Prints the new memory address, which is different from the previous one
