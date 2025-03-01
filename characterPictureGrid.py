255d5280eac6ffe0c46d75983ea461105691b64c
Ruman Dhaliwal <rumanddd@gmail.com>
2025-03-01 16:22:58 +0100

    .

---
+++ characterPictureGrid.py
@@ -0,0 +1,20 @@
+# This program illustrates the character picture grid
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+rows = len(grid) # Number of elements in the list
+cols = len(grid[0]) # Number of elements in every single element in the list
+
+for j in range(cols):
+    for i in range(rows):
+        print(grid[i][j], end='')
+    print()
