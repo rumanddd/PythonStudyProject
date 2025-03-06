8914bf19653dce6d5d4ca8219f27cccd2293b37e
Ruman Dhaliwal <rumanddd@gmail.com>
2025-03-06 21:05:17 +0100

    .

---
+++ birthdays.py
@@ -0,0 +1,14 @@
+birthdays = {'Ulviyya': '11th of September', 'Ruman': '30th of September', 'Pluto': '15th of June'}
+while True:
+    print('Enter a name: (blank to quit)')
+    name = input()
+    if name == '':
+        break
+    if name in birthdays:
+            print(name + "'s birthday is on " + birthdays[name])
+    else:
+            print('I do not have the birthday information for ' + name)
+            print('When is their birthday?')
+            bday = input()
+            birthdays[name] = bday
+            print('Birthday database updated.')
